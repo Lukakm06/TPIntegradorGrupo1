bbffb81ff9ff937048e1ad99572800326bd62bb1
Lukakm06 <126888856+Lukakm06@users.noreply.github.com>
2023-11-17 10:33:55 -0300

    nombres bien puestos

--- Códigos/Codigo_joystick_prueba/Codigo_joystick_prueba.ino
+++ Códigos/Codigo_joystick_prueba/Codigo_joystick_prueba.ino
@@ -1,52 +1,27 @@
-#include <LiquidCrystal_I2C.h>
+#define EJE_X 34
+#define EJE_Y 35
+#define BOTON 2
+int valorX;
+int valorY;
+int botonPulsado;
 
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
-
-
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
-
-
-String messageStatic = "Static message";
-String messageToScroll = "This is a scrolling message with more than 16 characters";
-
-
-// Function to scroll text
-// The function acepts the following arguments:
-// row: row number where the text will be displayed
-// message: message to scroll
-// delayTime: delay between each character shifting
-// lcdColumns: number of columns of your LCD
-void scrollText(int row, String message, int delayTime, int lcdColumns) {
-  for (int i=0; i < lcdColumns; i++) {
-    message = " " + message;  
-  } 
-  message = message + " "; 
-  for (int pos = 0; pos < message.length(); pos++) {
-    lcd.setCursor(0, row);
-    lcd.print(message.substring(pos, pos + lcdColumns));
-    delay(delayTime);
-  }
+void setup() {
+  Serial.begin(9600);
+  // Será de tipo entrada
+  pinMode(BOTON, INPUT_PULLUP);
 }
 
 
-void setup(){
-  // initialize LCD
-  lcd.init();
-  // turn on LCD backlight                      
-  lcd.backlight();
-}
-
-
-void loop(){
-  // set cursor to first column, first row
-  lcd.setCursor(0, 0);
-  // print static message
-  lcd.print(messageStatic);
-  // print scrolling message
-  scrollText(1, messageToScroll, 250, lcdColumns);
+void loop() {
+   valorX = analogRead(EJE_X),
+      valorY = analogRead(EJE_Y),
+      botonPulsado = digitalRead(BOTON);
+  Serial.print("\nValor X: ");
+  Serial.print(valorX);
+  Serial.print("\nValor Y: ");
+  Serial.print(valorY);
+  Serial.print("\nBotón pulsado: ");
+  Serial.print(botonPulsado);
+  delay(1000);
 }
